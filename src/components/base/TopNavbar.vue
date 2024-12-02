528e7bd7b1c116c4e8dea39cc70ae4896063d300
Sardor <matniyazov.sardor@fido.uz>
2024-12-02 09:59:44 +0500

    .. 2

--- src/components/base/TopNavbar.vue
+++ src/components/base/TopNavbar.vue
@@ -1,20 +1,19 @@
 <template>
   <div class="topbar">
-    <nav class="navbar-custom" id="navbar-custom">
+    <nav class="navbar-custom">
       <ul class="list-unstyled topbar-nav float-end mb-0">
         <li class="dropdown">
-          <a class="nav-link dropdown-toggle arrow-none nav-icon" data-bs-toggle="dropdown" href="#" role="button"
-             aria-haspopup="false" aria-expanded="false">
-            <img src="@/assets/logo.svg" alt="" class="thumb-xxs rounded-circle">
+          <a class="nav-link dropdown-toggle arrow-none nav-icon" href="#">
+            <img src="@/assets/images/sun.png" alt="" class="thumb-xxs rounded-circle">
           </a>
           <div class="dropdown-menu">
-            <a class="dropdown-item" href="#"><img src="@/assets/logo.svg" alt="" height="15"
+            <a class="dropdown-item" href="#"><img src="@/assets/images/sun.png" alt=""
                                                    class="me-2">English</a>
-            <a class="dropdown-item" href="#"><img src="@/assets/logo.svg" alt="" height="15"
+            <a class="dropdown-item" href="#"><img src="@/assets/images/sun.png" alt=""
                                                    class="me-2">Spanish</a>
-            <a class="dropdown-item" href="#"><img src="@/assets/logo.svg" alt="" height="15"
+            <a class="dropdown-item" href="#"><img src="@/assets/images/sun.png" alt=""
                                                    class="me-2">German</a>
-            <a class="dropdown-item" href="#"><img src="@/assets/logo.svg" alt="" height="15"
+            <a class="dropdown-item" href="#"><img src="@/assets/images/sun.png" alt=""
                                                    class="me-2">French</a>
           </div>
         </li>
@@ -34,7 +33,7 @@
                 <small class="float-end text-muted ps-2">2 min ago</small>
                 <div class="media">
                   <div class="avatar-md bg-soft-primary">
-                    <img src="@/assets/images/user/user-1.png" alt="" class="thumb-sm rounded-circle">
+                    <img src="@/assets/images/sun.png" alt="" class="thumb-sm rounded-circle">
                   </div>
                   <div class="media-body align-self-center ms-2 text-truncate">
                     <h6 class="my-0 fw-normal text-dark">Your order is placed</h6>
@@ -47,7 +46,7 @@
                 <small class="float-end text-muted ps-2">10 min ago</small>
                 <div class="media">
                   <div class="avatar-md bg-soft-primary">
-                    <img src="@/assets/logo.svg" alt="" class="thumb-sm rounded-circle">
+                    <img src="@/assets/images/sun.png" alt="" class="thumb-sm rounded-circle">
                   </div>
                   <div class="media-body align-self-center ms-2 text-truncate">
                     <h6 class="my-0 fw-normal text-dark">Meeting with designers</h6>
@@ -60,7 +59,7 @@
                 <small class="float-end text-muted ps-2">40 min ago</small>
                 <div class="media">
                   <div class="avatar-md bg-soft-primary">
-                    <img src="@/assets/logo.svg" alt="" class="thumb-sm rounded-circle">
+                    <img src="@/assets/images/sun.png" alt="" class="thumb-sm rounded-circle">
                   </div>
                   <div class="media-body align-self-center ms-2 text-truncate">
                     <h6 class="my-0 fw-normal text-dark">UX 3 Task complete.</h6>
@@ -73,7 +72,7 @@
                 <small class="float-end text-muted ps-2">1 hr ago</small>
                 <div class="media">
                   <div class="avatar-md bg-soft-primary">
-                    <img src="@/assets/logo.svg" alt="" class="thumb-sm rounded-circle">
+                    <img src="@/assets/images/sun.png" alt="" class="thumb-sm rounded-circle">
                   </div>
                   <div class="media-body align-self-center ms-2 text-truncate">
                     <h6 class="my-0 fw-normal text-dark">Your order is placed</h6>
@@ -86,7 +85,7 @@
                 <small class="float-end text-muted ps-2">2 hrs ago</small>
                 <div class="media">
                   <div class="avatar-md bg-soft-primary">
-                    <img src="@/assets/logo.svg" alt="" class="thumb-sm rounded-circle">
+                    <img src="@/assets/images/sun.png" alt="" class="thumb-sm rounded-circle">
                   </div>
                   <div class="media-body align-self-center ms-2 text-truncate">
                     <h6 class="my-0 fw-normal text-dark">Payment Successfull</h6>
@@ -96,16 +95,15 @@
               </a>
             </div>
 
-            <a href="javascript:void(0);" class="dropdown-item text-center text-primary">
+            <a href="" class="dropdown-item text-center text-primary">
               View all <i class="fi-arrow-right"></i>
             </a>
           </div>
         </li>
 
         <li class="dropdown notification-list">
-          <a class="nav-link dropdown-toggle arrow-none nav-icon" data-bs-toggle="dropdown" href="#" role="button"
-             aria-haspopup="false" aria-expanded="false">
-            <i class="ti ti-bell"></i>
+          <a class="nav-link dropdown-toggle arrow-none nav-icon" href="#" role="button">
+            <i class="mdi mdi-bell"></i>
             <span class="alert-badge"></span>
           </a>
           <div class="dropdown-menu dropdown-menu-end dropdown-lg pt-0">
@@ -113,13 +111,12 @@
             <h6 class="dropdown-item-text font-15 m-0 py-3 border-bottom d-flex justify-content-between align-items-center">
               Notifications <span class="badge bg-soft-primary badge-pill">2</span>
             </h6>
-            <div class="notification-menu" data-simplebar>
-
+            <div class="notification-menu">
               <a href="#" class="dropdown-item py-3">
                 <small class="float-end text-muted ps-2">2 min ago</small>
                 <div class="media">
                   <div class="avatar-md bg-soft-primary">
-                    <i class="ti ti-chart-arcs"></i>
+                    <i class="mdi mdi-chart-arcs"></i>
                   </div>
                   <div class="media-body align-self-center ms-2 text-truncate">
                     <h6 class="my-0 fw-normal text-dark">Your order is placed</h6>
@@ -132,7 +129,7 @@
                 <small class="float-end text-muted ps-2">10 min ago</small>
                 <div class="media">
                   <div class="avatar-md bg-soft-primary">
-                    <i class="ti ti-device-computer-camera"></i>
+                    <i class="mdi mdi-device-computer-camera"></i>
                   </div>
                   <div class="media-body align-self-center ms-2 text-truncate">
                     <h6 class="my-0 fw-normal text-dark">Meeting with designers</h6>
@@ -145,7 +142,7 @@
                 <small class="float-end text-muted ps-2">40 min ago</small>
                 <div class="media">
                   <div class="avatar-md bg-soft-primary">
-                    <i class="ti ti-diamond"></i>
+                    <i class="mdi mdi-diamond"></i>
                   </div>
                   <div class="media-body align-self-center ms-2 text-truncate">
                     <h6 class="my-0 fw-normal text-dark">UX 3 Task complete.</h6>
@@ -158,7 +155,7 @@
                 <small class="float-end text-muted ps-2">1 hr ago</small>
                 <div class="media">
                   <div class="avatar-md bg-soft-primary">
-                    <i class="ti ti-drone"></i>
+                    <i class="mdi mdi-drone"></i>
                   </div>
                   <div class="media-body align-self-center ms-2 text-truncate">
                     <h6 class="my-0 fw-normal text-dark">Your order is placed</h6>
@@ -171,7 +168,7 @@
                 <small class="float-end text-muted ps-2">2 hrs ago</small>
                 <div class="media">
                   <div class="avatar-md bg-soft-primary">
-                    <i class="ti ti-users"></i>
+                    <i class="mdi mdi-users"></i>
                   </div>
                   <div class="media-body align-self-center ms-2 text-truncate">
                     <h6 class="my-0 fw-normal text-dark">Payment Successfull</h6>
@@ -181,17 +178,16 @@
               </a>
             </div>
 
-            <a href="javascript:void(0);" class="dropdown-item text-center text-primary">
-              View all <i class="fi-arrow-right"></i>
+            <a href="" class="dropdown-item text-center text-primary">
+              View all <i class="mdi mdi-arrow-right"></i>
             </a>
           </div>
         </li>
 
         <li class="dropdown">
-          <a class="nav-link dropdown-toggle nav-user" data-bs-toggle="dropdown" href="#" role="button"
-             aria-haspopup="false" aria-expanded="false">
+          <a class="nav-link dropdown-toggle nav-user" href="#">
             <div class="d-flex align-items-center">
-              <img src="@/assets/logo.svg" alt="profile-user"
+              <img src="@/assets/images/sun.png" alt="profile-user"
                    class="rounded-circle me-2 thumb-sm"/>
               <div>
                 <small class="d-none d-md-block font-11">Admin</small>
@@ -201,31 +197,30 @@
             </div>
           </a>
           <div class="dropdown-menu dropdown-menu-end">
-            <a class="dropdown-item" href="#"><i class="ti ti-user font-16 me-1 align-text-bottom"></i> Profile</a>
-            <a class="dropdown-item" href="#"><i class="ti ti-settings font-16 me-1 align-text-bottom"></i>
+            <a class="dropdown-item" href="#"><i class="mdi mdi-user font-16 me-1 align-text-bottom"></i> Profile</a>
+            <a class="dropdown-item" href="#"><i class="mdi mdi-settings font-16 me-1 align-text-bottom"></i>
               Settings</a>
             <div class="dropdown-divider mb-0"></div>
-            <a class="dropdown-item" href="#"><i class="ti ti-power font-16 me-1 align-text-bottom"></i> Logout</a>
+            <a class="dropdown-item" href="#"><i class="mdi mdi-power font-16 me-1 align-text-bottom"></i> Logout</a>
           </div>
         </li>
         <li class="notification-list">
-          <a class="nav-link arrow-none nav-icon offcanvas-btn" href="#" data-bs-toggle="offcanvas"
-             data-bs-target="#Appearance" role="button" aria-controls="Rightbar">
-            <i class="ti ti-settings ti-spin"></i>
+          <a class="nav-link arrow-none nav-icon offcanvas-btn" href="#">
+            <i class="mdi mdi-settings mdi-spin"></i>
           </a>
         </li>
       </ul>
 
       <ul class="list-unstyled topbar-nav mb-0">
         <li>
-          <button class="nav-link button-menu-mobile nav-icon" id="togglemenu">
-            <i class="ti ti-menu-2"></i>
+          <button class="nav-link button-menu-mobile nav-icon">
+            <i class="mdi mdi-menu"></i>
           </button>
         </li>
         <li class="hide-phone app-search">
-          <form role="search" action="#" method="get">
-            <input type="search" name="search" class="form-control top-search mb-0" placeholder="Type text...">
-            <button type="submit"><i class="ti ti-search"></i></button>
+          <form role="search" action="#">
+            <input type="search" name="search" class="form-control top-search mb-0" placeholder="Search something...">
+            <button type="submit"><i class="mdi mdi-search"></i></button>
           </form>
         </li>
       </ul>
